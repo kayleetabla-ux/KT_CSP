75dfe19eb8a6db1673e8cda12df6a9f30f774be3
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-09 13:52:50 -0600

    variable notes

---
+++ variable_notes.py
@@ -0,0 +1,7 @@
+#KT, variables notes
+
+name = input("Tell me your name:")
+
+print("CSP is the best class!")
+print("it is taught by the coolest teacher")
+print("hello", name, "welcome to class") 
