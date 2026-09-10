af90cb282aa61070f26568ac5df128c88270a2c4
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-10 13:47:38 -0600

    variable practice

---
+++ variables_practice.py
@@ -0,0 +1,31 @@
+#KT, Variables_practice.py
+
+name = input("tell me your name")
+print("Hello", name)
+
+number_1to10 = input("tell me a number 1 through 10")
+print(number_1to10, ", ok")
+
+number_100to1000 = input("now tell me a number 100 through 1000")
+print(number_100to1000)
+
+breakfast = input("what did you have for breakfast?")
+print(breakfast)
+
+fav_color = input("what is your favorite color?")
+print(fav_color)
+
+school = "UCAS"
+print("I go to", school)
+
+year = input("what year is it?")
+print(year)
+
+eye_color = input ("whats your eye color?")
+print(eye_color)
+
+someones_age = "16"
+print(someones_age)
+
+fav_subject = input("what's your favorite subject in school?")
+print("I like", fav_subject, "too")
