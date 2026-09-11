5c1b1dbf07079c17bd4dd4c71e244a6af3c9ae93
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-11 13:38:29 -0600

    physical_logical_memoery.py

--- physical_logical_memoery.py
+++ physical_logical_memoery.py
@@ -6,4 +6,4 @@ everything = 42
 
 
 print(f"the variable pi is {pi} and it is saved at {id(pi)}")
-print(f"the variable everything is {everything} and it is saved at {id(everything)}")
+print(f"the variable  is {everything} and it is saved at {id(everything)}")
