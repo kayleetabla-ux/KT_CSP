28352d479d43d5b993f5f7b008df314dd9ebb192
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-11 13:35:45 -0600

    physical_logical_memoery.py

---
+++ physical_logical_memoery.py
@@ -0,0 +1,9 @@
+#KT, physical and logical memoery
+
+pi = 3.1415
+
+everything = 42
+
+
+print(f"the variable pi is {pi} and it is saved at {id(pi)}")
+print(f"the variable everything is {everything} and it is saved at {id(everything)}")
