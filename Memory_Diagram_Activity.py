755202fafbbec644f2be367c0fab240eb94cfa5a
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-11 13:52:46 -0600

    Memory_Diagram_Activity.py

---
+++ Memory_Diagram_Activity.py
@@ -0,0 +1,14 @@
+#KT, Memory Diagram Activity
+
+name = "Alex"
+age = 14
+
+print(f"{name} saved at {id(name)}")
+print(f"{age} saved at {id(age)}")
+
+score = 10
+score = 15
+
+print(f"{score} saved at {id(score)}")
+print(f)
+
