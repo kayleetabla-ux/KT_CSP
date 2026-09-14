466c75c855ab88d8ff2fbb1c5e500017f55bbbff
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-14 13:51:42 -0600

    Memory_Diagram_Activity.py

--- Memory_Diagram_Activity.py
+++ Memory_Diagram_Activity.py
@@ -10,5 +10,20 @@ score = 10
 score = 15
 
 print(f"{score} saved at {id(score)}")
-print(f)
+print(f"{score} saved at {id(score)}")
+
+height = 5.9
+city = "Denver"
+zip_code = 80202
+
+print(f"{height} saved at {id(height)}")
+print(f"{city} saved at {id(city)}")
+print(f"{zip_code} saved at {id(zip_code)}")
+
+price1 = 10
+price2 = 10
+
+print(f"{price1} saved at {id(price1)}")
+print(f"{price2} saved at {id(price2)}")
+
 
