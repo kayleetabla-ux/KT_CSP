d4ba5ff5d488a1e9b44130a1f8393339d3655170
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-15 19:44:22 +0000

    hello_user.py

---
+++ hello_user.py
@@ -0,0 +1,11 @@
+#KT, hello user
+
+while True:
+    name = input("tell me your name")
+    if name.isneumeric():
+        print("that isn't a number")
+    else:
+        break
+
+print("hello {name}!")
+
