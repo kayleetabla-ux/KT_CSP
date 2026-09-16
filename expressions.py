a214763200e84ef2020dad6de431aff52dddaee5
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-16 13:53:03 -0600

    expression.py

---
+++ expressions.py
@@ -0,0 +1,28 @@
+#KT, integers, floats and expressions notes
+
+# Integers
+people = 18
+
+shoes = people * 2
+
+hand = people * 2
+
+# Float
+pi = 3.1415
+
+water = 60.5
+
+temp = 68.5
+
+price = 1.99
+
+# Expressions
+print(water * price)
+print(f"your {water} gallons of water costs ${round(water * price,2)}")
+
+#modulo amd integer division
+print(f"18/4 ={18/4} which is {18//4} whith a remainder of {18%4}")
+print(f"18/5 ={18/5} which is {18//5} whith a remainder of {18%5}")
+print(f"18/6 ={18/6} which is {18//6} whith a remainder of {18%6}")
+
+print(int(people*2*(5-1)/3))
