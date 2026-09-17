900cf3532db0c1e2a6308be4e877b8fea13f084e
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-17 13:50:31 -0600

    your_budget.py

---
+++ your_budget.py
@@ -0,0 +1,22 @@
+#KT, Your Budget
+
+income = (float(input("what is your monthly income?:")))
+print(income)
+
+rent_mortgage = (float(input("what is your monthly rent/mortgage?:")))
+print(rent_mortgage)
+
+utilities = (float(input("what is your monthly utilities?:")))
+print(utilities)
+
+groceries = (float(input("what is your monthly grocieries?:")))
+print(groceries)
+
+transport = (float(input("what is your monthly transportation?:")))
+
+print(f"your rent is ${rent_mortgage:.2f} and that is {int(round(rent_mortgage/income)*100)}% of your income")
+print(f"your rent is ${utilities:.2f} and that is {int(round(utilities/income)*100)}% of your income")
+
+print(f"your rent is ${groceries:.2f} and that is {int(round(groceries/income)*100)}% of your income")
+
+print(f"your rent is ${transport:.2f} and that is {int(round(transport/income)*100)}% of your income")
