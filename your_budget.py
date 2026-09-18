ef5804d1ffa3298fc99dc7d8d725fe661e844c46
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-18 12:22:26 -0600

    your_budget.py

--- your_budget.py
+++ your_budget.py
@@ -14,9 +14,13 @@ print(groceries)
 
 transport = (float(input("what is your monthly transportation?:")))
 
-print(f"your rent is ${rent_mortgage:.2f} and that is {int(round(rent_mortgage/income)*100)}% of your income")
-print(f"your rent is ${utilities:.2f} and that is {int(round(utilities/income)*100)}% of your income")
+print(f"your rent is ${rent_mortgage:.2f} and that is {int(round((rent_mortgage/income)*100))}% of your income")
 
-print(f"your rent is ${groceries:.2f} and that is {int(round(groceries/income)*100)}% of your income")
+print(f"your rent is ${utilities:.2f} and that is {int(round((utilities/income)*100))}% of your income")
 
-print(f"your rent is ${transport:.2f} and that is {int(round(transport/income)*100)}% of your income")
+print(f"your rent is ${groceries:.2f} and that is {int(round((groceries/income)*100))}% of your income")
+
+print(f"your rent is ${transport:.2f} and that is {int(round((transport/income)*100))}% of your income")
+
+print("you should save $300.00 a month, that's 10% of your income")
+
