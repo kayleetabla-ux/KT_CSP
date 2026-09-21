dde044ee56daaf51f30ac168aaef251c37fe0d09
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-21 13:35:17 -0600

    silly_sentences.py

---
+++ silly_sentences.py
@@ -0,0 +1,16 @@
+#KT, silly sentences
+
+adjective = input("give me an adjective:")
+
+animal = input("give me an animal:")
+
+plural_noun = input("give me a plural noun:")
+
+verb = input("give me a verb:")
+
+food = input("give me a food:")
+
+exclamation = input("give me an exclamation:")
+
+print("Yesterday, I decided to walk my pet "+animal+" down the street. It was a very "+adjective+" morning until we ran into a giant pile of "+plural_noun+". My pet immediately started to "+verb+" all over them! To calm it down, I offered it a delicious piece of "
++food+". It took one look, spat it out, and yelled, "+exclamation+"!")
