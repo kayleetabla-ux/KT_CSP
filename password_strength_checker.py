5b0c755a99865673168e29ec85f4168108384306
kayleetabla-ux <kaylee.tabla@ucas-edu.net>
2026-09-24 00:49:02 +0000

    password_strength_checker.py

--- password_strength_checker.py
+++ password_strength_checker.py
@@ -7,7 +7,7 @@ lowercase = False
 number = False
 symbol = False
 length = False
-
+increment = 0
 if len (password)>=8:
     length = True
     print(f"at least 8 characters long: {length}")
@@ -28,4 +28,20 @@ print(f"Has a lowercase letter: {lowercase}")
 print(f"Has an uppercase letter: {uppercase}")
 print(f"Has a number: {number}")
 print(f"Has a symbol: {symbol}")
-
+if {password}==True:
+    ({increment}+1)
+if {upercase}==True:
+    ({increment}+1)
+if {lowercase}==True:
+    ({increment}+1)
+if {number}==True:
+    ({increment}+1)
+if {symbol}==True:
+    ({increment}+1)
+if {increment}==5:
+    print("your password strength is: medium")
+if {increment}<=2:
+    print("your password strength is: weak")
+if {increment}==3 or 4:
+    print("your password stregth is: strong")
+    
